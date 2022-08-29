18f780ce0b451deb326881ca33d9dc89fc6296c2
Tony JinHuiLiu <20090185@mail.wit.ie>
2022-08-29 14:48:00 +0100

    Topic 10 - Update Movies Data/Route and noticed a error where i lift out the loadgenre

--- movies-api/seedData/index.js
+++ movies-api/seedData/index.js
@@ -1,6 +1,8 @@
 import userModel from '../api/users/userModel';
 import users from './users';
 import dotenv from 'dotenv';
+import movieModel from '../api/movies/movieModel';
+import movies from './movies.js';
 
 dotenv.config();
 
@@ -15,7 +17,34 @@ async function loadUsers() {
       console.error(`failed to Load user Data: ${err}`);
     }
   }
-
-if (process.env.SEED_DB) {
-  loadUsers();
-}
+  
+  // deletes all user documents in collection and inserts test data
+  async function loadGenres() {
+    console.log('load genre Data');
+    try {
+      await genreModel.deleteMany();
+      await genreModel.collection.insertMany(genres);
+      console.info(`${genres.length} genres were successfully stored.`);
+    } catch (err) {
+      console.error(`failed to Load genre Data: ${err}`);
+    }
+  }
+  
+  // deletes all movies documents in collection and inserts test data
+  export async function loadMovies() {
+    console.log('load seed data');
+    console.log(movies.length);
+    try {
+      await movieModel.deleteMany();
+      await movieModel.collection.insertMany(movies);
+      console.info(`${movies.length} Movies were successfully stored.`);
+    } catch (err) {
+      console.error(`failed to Load movie Data: ${err}`);
+    }
+  }
+  
+  if (process.env.SEED_DB) {
+    loadUsers();
+    loadGenres();
+    loadMovies();
+  }
